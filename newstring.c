7e93362be306e313fab28ee2581310c655347b9a
Lucas Pyaia <lucaspyaia@hotmail.com>
2018-03-10 12:04:08 -0300

    added newstring.c file

---
+++ newstring.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+size_t my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
+
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Tutorials"
+   };
+
+   return 0;
+}
